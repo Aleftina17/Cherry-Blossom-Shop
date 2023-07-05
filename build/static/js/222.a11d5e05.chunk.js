de3c49144ced32e49bf0d00a078586990b0dc315
Aleftina17 <124050765+Aleftina17@users.noreply.github.com>
2023-07-05 16:40:38 +0300

    Delete build directory

--- build/static/js/222.a11d5e05.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkflowers_react=self.webpackChunkflowers_react||[]).push([[222],{5222:function(e,n,r){r.r(n),r.d(n,{default:function(){return c}});var t=r(2791),u=r(7689);function c(){var e=(0,u.TH)().pathname;return(0,t.useEffect)((function(){window.scrollTo(0,0)}),[e]),null}}}]);
-//# sourceMappingURL=222.a11d5e05.chunk.js.map
