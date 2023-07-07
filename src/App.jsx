c69587795f1c43d01f9d661d190b5fc7a6ce808c
Aleftina17 <teekupinmuotti@gmail.com>
2023-07-07 16:57:13 +0300

    Browser Router changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import "./App.scss";
 import "./common.scss";
@@ -33,26 +33,24 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <ScrollToTop />
+      <ScrollToTop />
 
-        <Header toggleMenu={toggleMenu} />
-        <Menu isOpen={isMenuOpen} closeMenu={closeMenu} />
-        
-        <Suspense fallback={<h3>Loading...</h3>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/about-us" element={<AboutUsPage />} />
-            <Route path="/collections" element={<CollectionsPage />} />
-            <Route path="/catalog" element={<CatalogPage />} />
-            <Route path="/contacts" element={<ContactsPage />} />
-            <Route path="/product/:id" element={<ProductPage />} />
-          </Routes>
-        </Suspense>
-        <Footer />
-      </BrowserRouter>
+      <Header toggleMenu={toggleMenu} />
+      <Menu isOpen={isMenuOpen} closeMenu={closeMenu} />
+
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/collections" element={<CollectionsPage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+        </Routes>
+      </Suspense>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import { CartProvider } from './components/CartContext/CartContext';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import { CartProvider } from "./components/CartContext/CartContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <BrowserRouter>
     <CartProvider>
       <App />
     </CartProvider>
-  
+  </BrowserRouter>
 );
-
-
