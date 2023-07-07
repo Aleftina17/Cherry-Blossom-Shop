b4d9ff90152f7b61921714503671d3e440d428ca
Aleftina17 <teekupinmuotti@gmail.com>
2023-07-07 17:12:07 +0300

    HashRouter added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
@@ -7,9 +7,9 @@ import { CartProvider } from "./components/CartContext/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <CartProvider>
       <App />
     </CartProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
