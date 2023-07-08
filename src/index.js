a1062a89da8d2d5d4e67f5c680b506c7032266a1
Aleftina17 <teekupinmuotti@gmail.com>
2023-07-08 15:44:37 +0300

    BrowserRouter changed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
@@ -7,9 +7,9 @@ import { CartProvider } from "./components/CartContext/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <CartProvider>
       <App />
     </CartProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
